759a7caf2d81abbe64937c146c1be4215bbccb79
happen-zhang <zhanghaipeng404@gmail.com>
2014-09-22 13:02:39 +0800

    Add admin view page.

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,10 +7,29 @@ var adminControllers;
 
 adminControllers = {
 
-    index: function (req, res, next) {
-        res.render('index', { title: 'Hello World!' });
-    }
+    index: function(req, res, next) {
+        res.render('admin/index');
+    },
 
+    addJob: function(req, res, next) {
+        res.render('admin/add_job');
+    },
+
+    editJob: function(req, res, next) {
+        res.render('admin/edit_job');
+    },
+
+    type: function(req, res, next) {
+        res.render('admin/type');
+    },
+
+    addType: function(req, res, next) {
+        res.render('admin/add_type');
+    },
+
+    editType: function(req, res, next) {
+        res.render('admin/edit)type');
+    }
 };
 
 module.exports = adminControllers;
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,17 @@ var admin = require('../controllers/admin'),
     adminRoutes;
 
 adminRoutes = function (app) {
-    app.get('/admin/', admin.index);
+    app.get('/admin', admin.index);
+
+    app.get('/admin/add_job', admin.addJob);
+
+    app.get('/admin/edit_job', admin.editJob);
+
+    app.get('/admin/type', admin.type);
+
+    app.get('/admin/add_type', admin.addType);
+
+    app.get('/admin/edit_type', admin.editType);
 };
 
 module.exports = adminRoutes;
